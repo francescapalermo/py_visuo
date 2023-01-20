3ecf6b484dba8f0897df9197756d700613478b76
Francesca <catminu@hotmail.it>
2023-01-20 14:17:17 +0000

    updated setup

--- setup.py
+++ setup.py
@@ -6,34 +6,15 @@ import logging
 
 PACKAGE_NAME = 'py_visuo'
 
-
-# setup(
-#     name=__title__,
-#     version=__version__,
-#     description=__doc__,
-#     author=__author__,
-#     author_email=__author_email__,
-#     packages=find_packages(),
-#     long_description=open('README.txt').read(),
-#     install_requires=[
-#                         "numpy>=1.22",
-#                         "pandas>=1.4",
-#                         "matplotlib>=3.5",
-#                         "seaborn>=0.11.2",
-#                         "tqdm>=4.64",
-#     ]
-# )
-
-
 setup(
     name='py_visuo',
     version='0.0.1',    
     description='Collection of functions and classes for creating beautiful and informative plots in Python',
-    url='https://github.com/francescapalermo/py_visuo/',
+    url='https://github.com/francescapalermo/py_visuo',
     author='Francesca Palermo',
     author_email='f.palermo@imperial.ac.uk',
     license='MIT-License',
-    packages=find_packages(),
+    packages=['py_visuo'],#find_packages(),
     install_requires=[  "numpy>=1.22",
                         "pandas>=1.4",
                         "matplotlib>=3.5",
