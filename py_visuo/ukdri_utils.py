899cc0917eb6855a9dcc07ef5e30694a4cafeb7a
Francesca <catminu@hotmail.it>
2023-03-28 15:02:59 +0100

    updated ukdri-activity plots

--- py_visuo/ukdri_utils.py
+++ py_visuo/ukdri_utils.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 import matplotlib.dates as mdates
-import visualise
-import custom_colors
+from py_visuo import visualise
+from py_visuo import custom_colors
 
 
-def create_activity_plot(df, patient_list, month_list, location_list, column_patient='patient_id', column_date='date',
+def create_activity_plot(df, patient_list, month_list, year, location_list, column_patient='patient_id', column_date='date',
                          palette=custom_colors.ukdri_movement, max_yticks=15, savefig=True, save_path='./'):
     for patient in patient_list:
         for month in month_list:
@@ -14,12 +14,13 @@ def create_activity_plot(df, patient_list, month_list, location_list, column_pat
             for location in location_list:
                 current_df = df[df[column_patient] ==
                                 patient].reset_index(drop=True)
-                current_df = current_df[current_df[column_date].dt.month.isin(
-                    month)].reset_index(drop=True)
+                current_df = current_df[current_df[column_date].dt.month.isin(month)].reset_index(drop=True)
+                current_df = current_df[current_df[column_date].dt.year==year].reset_index(drop=True)
+
                 current_df['date_more'] = current_df[column_date].astype(
                     str).str.split().str[0]
                 current_df['time_of_day'] = pd.to_datetime(
-                    '2000-01-01 ' + current_df['date'].dt.time.astype(str))
+                    '2000-01-01 ' + current_df[column_date].dt.time.astype(str))
                 tmp_input = current_df.query('location_name == @location')
                 tmp_input = tmp_input.sort_values(
                     by=['date_more'], ascending=False)
@@ -42,17 +43,20 @@ def create_activity_plot(df, patient_list, month_list, location_list, column_pat
             if savefig:
                 plt.savefig(save_path + '{}_activity.svg'.format(patient))
 
+def test():
+    activity_df = pd.read_csv('./Activity.csv')
+    activity_df['date'] = pd.to_datetime(activity_df['date'])
+    print(activity_df['location_name'].unique())
+    activity_df = activity_df.sort_values(by=['patient_id', 'date']).reset_index(drop=True)
+    location_list = ['Kitchen', 'Lounge', 'Bedroom', 'Bathroom', 'Front door']#'Fridge', 'Kettle', 'Toaster', 'Microwave','Front door']
+    month_list = [[4, 5, 6]]
+    year = 2019
+    patient_list = ['73f7c']
 
-activity_df = pd.read_csv('./data/Activity.csv')
-activity_df['date'] = pd.to_datetime(activity_df['date'])
-print(activity_df['location_name'].unique())
-activity_df = activity_df.sort_values(by=['patient_id', 'date']).reset_index(drop=True)
-location_list = ['Kitchen', 'Lounge', 'Bedroom', 'Bathroom', 'Front door']#'Fridge', 'Kettle', 'Toaster', 'Microwave','Front door']
-month_list = [[4, 5, 6]]
-patient_list = ['73f7c']
+    create_activity_plot(activity_df, patient_list, month_list, year, location_list, column_patient='patient_id', column_date='date',
+                            palette=custom_colors.ukdri_movement, max_yticks=15, savefig=False)
 
-create_activity_plot(activity_df, patient_list, month_list, location_list, column_patient='patient_id', column_date='date',
-                         palette=custom_colors.ukdri_movement, max_yticks=15, savefig=False)
+    plt.show()
 
-plt.show()
-print('Test')
+if __name__ == '__main__':
+    test()
